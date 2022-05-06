2e9a458666108e840f62e359a22758e1711c9eeb
Thy Huynh <khanhhthyy99@gmail.com>
2022-05-05 20:29:49 -0700

    ver 1.3

--- index.js
+++ index.js
@@ -36,18 +36,6 @@ app.get("/", (req, res) => {
     //res.send("Root resource - Up and running!")
     res.render("index");
 });
-dblib.getTotalRecords()
-    .then(result => {
-        if (result.msg.substring(0, 5) === "Error") {
-            console.log(`Error Encountered.  ${result.msg}`);
-        } else {
-            console.log(`Total number of database records: ${result.totRecords}`);
-        };
-    })
-    .catch(err => {
-        console.log(`Error: ${err.message}`);
-    });
-
 
 app.get("/manage", async (req, res) => {
         // Omitted validation check
